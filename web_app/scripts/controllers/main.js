f0a7f3fd27d7939c49bca9a18d7cee835fdcdf64
holbrook <wanghaikuo@gmail.com>
2015-01-24 10:01:40 +0800

    update main

---
+++ web_app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngntsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngntsApp
+ */
+angular.module('ngntsApp')
+  .controller('MainCtrl', function () {
+    console.log('main controller');
+
+
+
+
+  });
